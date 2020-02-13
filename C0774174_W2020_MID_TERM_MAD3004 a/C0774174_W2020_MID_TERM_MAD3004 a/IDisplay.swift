3e66063a81e3ea4062127fa82877457d1ae3e3d6
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:59:33 -0500

    [ikroop] IDisplay class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/IDisplay.swift
@@ -0,0 +1,10 @@
+//
+//  IDisplay.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+

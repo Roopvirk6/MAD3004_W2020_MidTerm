bb07f3d050cb7817d5c94fcbcf7de9c38df6c6d9
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:29:19 -0500

    [IKROOP] Enum created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Enum.swift
@@ -0,0 +1,17 @@
+//
+//  Enum.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public struct Enum
+{
+    enum type
+    {
+        case MOBILE, HYDRO, INTERNET
+    }
+}

626a82a4078c4e18c7ab9dd8f6ae7ea8fc5af8c0
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:28:20 -0500

    [IKROOP] Bill class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Bill.swift
@@ -0,0 +1,21 @@
+//
+//  Bill.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Bill 
+{
+
+    var billId : Int
+    var billDate : Date
+    var billType : Enum.type
+    var totalBillAmount : Double
+    
+    
+    
+}

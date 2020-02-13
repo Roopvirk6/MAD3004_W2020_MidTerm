fcdcfaf54b27a430d2fd9aee48522c747dca14dd
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:34:57 -0500

    [IKROOP] Internet class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Internet.swift
@@ -0,0 +1,17 @@
+//
+//  Internet.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Internet
+{
+    var providerName : String
+    var internetGBUsed : Int
+    
+    
+}

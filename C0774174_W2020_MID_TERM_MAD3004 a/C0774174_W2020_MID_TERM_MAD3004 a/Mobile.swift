2dde26a81a744db3ef0c1f04a1230a89146d94f5
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:50:37 -0500

    [ikroop] mobile class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Mobile.swift
@@ -0,0 +1,24 @@
+//
+//  Mobile.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Mobile
+{
+    var mobileManufacturerName : String
+    var planName : String
+    var mobileNumber : Int
+    var internetGBUsed : Int
+    var minutes : Float
+    
+    
+    
+    
+    
+    
+}

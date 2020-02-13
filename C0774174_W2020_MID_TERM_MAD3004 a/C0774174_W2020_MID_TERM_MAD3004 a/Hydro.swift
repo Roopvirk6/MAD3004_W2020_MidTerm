f74f671b55558d4b2cd73b07d43c2c4a5861f037
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:38:56 -0500

    [IKROOP] Hydro class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Hydro.swift
@@ -0,0 +1,17 @@
+//
+//  Hydro.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Hydro
+{
+    var agencyName : String
+    var unitsConsumed : Double 
+    
+    
+}

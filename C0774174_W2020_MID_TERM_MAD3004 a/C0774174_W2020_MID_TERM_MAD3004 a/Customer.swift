142de59618dbe0bde6a61b2cab0f6769b1d07ed7
ikroop virk <virkikroop13@gmail.com>
2020-02-13 18:53:22 -0500

    [IKROOP] Customer class created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Customer.swift
@@ -0,0 +1,17 @@
+//
+//  Customer.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+public class Customer
+{
+    var customerId : String
+    var firstName : String
+    
+    
+}

b515e9ab0db90e5f0280fed6397dce2a7c585222
Ikroop <virkikroop13@gmail.com>
2020-02-14 18:07:50 -0500

    [Ikroop] Main class: Sample commit

--- C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/main.swift
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/main.swift
@@ -2,11 +2,9 @@
 //  main.swift
 //  C0774174_W2020_MID_TERM_MAD3004 a
 //
-//  Created by MacStudent on 2020-02-13.
+//  Created by MacStudent on 2020-02-14.
 //  Copyright © 2020 MacStudent. All rights reserved.
 //
 
 import Foundation
-
-
-
+  // sample commit

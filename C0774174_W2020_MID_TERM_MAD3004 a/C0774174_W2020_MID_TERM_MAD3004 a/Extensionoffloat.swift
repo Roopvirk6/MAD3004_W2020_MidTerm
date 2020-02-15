717350ffda4a0f271dcc011ddb1f79d75e97232e
ikroop virk <virkikroop13@gmail.com>
2020-02-14 20:40:42 -0500

    [IKROOP] Extension of float created

---
+++ C0774174_W2020_MID_TERM_MAD3004 a/C0774174_W2020_MID_TERM_MAD3004 a/Extensionoffloat.swift
@@ -0,0 +1,14 @@
+//
+//  Extensionofcurrency.swift
+//  C0774174_W2020_MID_TERM_MAD3004 a
+//
+//  Created by MacStudent on 2020-02-14.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+extension Float
+{
+    
+}
